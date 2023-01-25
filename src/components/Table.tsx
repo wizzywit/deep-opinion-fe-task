8cf8b3acbaa162cdd36e965a87d8d191530aa681
wisdompraise <wisdompraise968@gmail.com>
2023-01-25 13:25:16 +0100

    Implemented the virtualized table component.

---
+++ src/components/Table.tsx
@@ -0,0 +1,97 @@
+import {useState, UIEvent, useCallback} from "react";
+
+interface OwnProps {
+    rowHeight: number;
+    tableHeight: number;
+    rows: {[key: string]: string}[]
+}
+const Table = ({rowHeight = 35, tableHeight = 200, rows}: OwnProps) => {
+    const columns = Object.keys(rows[0])
+    const stateTableHeight = (rowHeight * rows.length)
+    const [scroll, setScroll] = useState({
+        top: 0,
+        index: 0,
+        end: Math.ceil((tableHeight * 2) / rowHeight)
+    })
+
+    const onScroll = useCallback(({ currentTarget }: UIEvent<HTMLTableElement>) => {
+
+        let scrollTop = currentTarget.scrollTop
+        let index = Math.floor(scrollTop / rowHeight)
+
+        const newScroll = {
+            index,
+            end: index + Math.ceil((tableHeight * 2) / rowHeight),
+            top: (scrollTop / rowHeight) * rowHeight
+        }
+
+        setScroll(() => newScroll);
+    },[rowHeight, tableHeight])
+
+    const generateRows = useCallback(() => {
+        let index = scroll.index
+        const end = scroll.end
+        let items = []
+
+        do {
+            if (index >= rows.length) {
+                index = rows.length
+                break
+            }
+
+            items.push(
+                <tr className={`tr ${(index % 2) === 0 ? 'tr-odd' : 'tr-even'}`} style={{
+                    position: "absolute",
+                    top: (index * rowHeight),
+                    left: 0,
+                    height: rowHeight,
+                    lineHeight: `${rowHeight}px`
+                }} key={index}>
+                    {columns.map((column, i) =>
+                        <td key={i}>
+                            {rows[index][column]}
+                        </td>
+                    )}
+                </tr>
+            )
+
+            index++
+        } while (index < end)
+
+        return items
+    },[columns, rowHeight, rows, scroll.end, scroll.index])
+
+    const currentTableHeight = (tableHeight > stateTableHeight)
+        ? stateTableHeight + 2
+        : tableHeight
+
+
+    return (
+        <div className={"wrapper"}>
+            <table>
+                <thead>
+                <tr className={'tr'}>
+                    {columns?.map((name, i) =>
+                        <th key={i}>{name}</th>
+                    )}
+                </tr>
+                </thead>
+            </table>
+            <table className='table-content' style={{
+                height: currentTableHeight
+            }} onScroll={onScroll}>
+                <tbody style={{
+                    position: "relative",
+                    display: 'inline-block',
+                    height: stateTableHeight,
+                    maxHeight: stateTableHeight,
+                    width: "100%"
+                }}>
+                {generateRows()}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Table
